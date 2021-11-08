ff04248c4f86691db1fe18c90bd43847beeb8263
Lala-banners <lala.l.banno@hotmail.com>
2021-11-08 15:04:53 +1100

    Fish jump anim + script activate

---
+++ Assets/Scripts/Creature Behaviours/OceanAIJump.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class OceanAIJump : MonoBehaviour
+{
+    private Animator fishAnim;
+    [SerializeField] private bool jump;
+
+    private void Start()
+    {
+        fishAnim = GetComponent<Animator>();
+        jump = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            jump = true;
+
+        if (jump.Equals(true))
+            fishAnim.SetBool("IsJumping", true);
+
+        if (jump.Equals(false))
+            fishAnim.SetBool("IsJumping", false);
+    }
+}

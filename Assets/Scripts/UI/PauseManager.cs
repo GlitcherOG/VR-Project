461c409a979533815283129fcef363d745f74aa8
Lala-banners <lala.l.banno@hotmail.com>
2021-11-09 21:08:37 +1100

    Done logic for pause menu - will link to radial menu

--- Assets/Scripts/UI/PauseManager.cs
+++ Assets/Scripts/UI/PauseManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PauseManager : MonoBehaviour
 {
+    public bool isGamePaused;
+    public GameObject pauseMenu;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,27 @@ public class PauseManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isGamePaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
     }
 }

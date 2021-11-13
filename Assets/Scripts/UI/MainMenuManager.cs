940c9367a496837bcc7a8fcbbbce9bc398922c20
Lala-banners <lala.l.banno@hotmail.com>
2021-11-14 07:01:51 +1100

    Testing menus (main and options)

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public GameObject mainMenu, optionMenu;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        optionMenu.SetActive(false);
+        mainMenu.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    //Close the app
+    public void QuitGame()
     {
-        
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#endif
+        Application.Quit();
     }
 }
--- Assets/Scripts/UI/OptionsManager.cs
+++ Assets/Scripts/UI/OptionsManager.cs
@@ -1,18 +1,78 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Toggle muteToggle;
+    public AudioMixer masterAudio;
+    public Slider musicSlider;
+    public Slider SFXSlider;
 
     // Update is called once per frame
     void Update()
     {
-        
+        LoadPlayerPrefs();
+    }
+    
+    //This changes volume in options
+    public void SetMusicVolume(float MusicVol)
+    {
+        masterAudio.SetFloat("MusicVol", MusicVol);
+    }
+    //This changes sound effects volume 
+    public void SetSFXVolume(float SFXVol)
+    {
+        masterAudio.SetFloat("SFXVol", SFXVol);
+    }
+
+    //Function to mute volume when toggle is active
+    public void ToggleMute(bool isMuted)
+    {
+        //string reference isMuted connects to the AudioMixer master group Volume and isMuted parameters in Unity
+        if (isMuted)
+        {
+            //-80 is the minimum volume
+            masterAudio.SetFloat("isMutedVolume", -80);
+        }
+        else
+        {
+            //20 is the maximum volume
+            masterAudio.SetFloat("isMutedVolume", 20);
+        }
+    }
+    
+    public void SavePlayerPrefs()
+    {
+        //save audio sliders
+        float musicVol;
+        if (masterAudio.GetFloat("MusicVol", out musicVol))
+        {
+            PlayerPrefs.SetFloat("MusicVol", musicVol);
+        }
+        float SFXVol;
+        if (masterAudio.GetFloat("SFXVol", out SFXVol))
+        {
+            PlayerPrefs.SetFloat("SFXVol", SFXVol);
+        }
+
+        PlayerPrefs.Save();
+    }
+    
+    public void LoadPlayerPrefs()
+    {
+        //load audio Sliders
+        if (PlayerPrefs.HasKey("MusicVol"))
+        {
+            float musicVol = PlayerPrefs.GetFloat("MusicVol");
+            musicSlider.value = musicVol;
+            masterAudio.SetFloat("MusicVol", musicVol);
+        }
+        if (PlayerPrefs.HasKey("SFXVol"))
+        {
+            float SFXVol = PlayerPrefs.GetFloat("SFXVol");
+            SFXSlider.value = SFXVol;
+            masterAudio.SetFloat("SFXVol", SFXVol);
+        }
     }
 }

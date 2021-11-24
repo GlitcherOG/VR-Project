80b62053418610a77227272e3c90730620dff5df
Jordy3D <fchetar@gmail.com>
2021-11-24 14:54:30 +1100

    Rotational UI, Bungalow Fix, New Menu Font, New Menu Skybox

--- Assets/Scripts/UI/PauseManager.cs
+++ Assets/Scripts/UI/PauseManager.cs
@@ -7,6 +7,8 @@ public class PauseManager : MonoBehaviour
   public bool isGamePaused;
   public GameObject pauseMenu;
 
+  Transform parent, cam;
+
   private void Awake()
   {
     pauseMenu = gameObject;
@@ -22,6 +24,19 @@ public class PauseManager : MonoBehaviour
 
   public void Pause()
   {
+    // Cache camera and parent
+    if (!cam) 
+      cam = Camera.main.transform;
+    if (!parent)
+      parent = transform.parent;
+
+    // Get camera rotation and position and apply appropriate values to UI parent
+    Vector3 newRot = new Vector3(0, cam.localEulerAngles.y, 0);
+    parent.localEulerAngles = newRot;
+    Vector3 newPos = new Vector3(cam.localPosition.x, 0, cam.localPosition.z);
+    parent.localPosition = newPos;
+
+    // Enable the UI
     pauseMenu.SetActive(true);
     //Time.timeScale = 0f;
     isGamePaused = true;

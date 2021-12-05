202760439a8b9cd413c05b09c2af4e85d2ec9cd7
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2021-12-06 09:46:32 +1100

    Added Sounds and Fixed Instant Popup

--- Assets/Scripts/Gaze/CameraDot.cs
+++ Assets/Scripts/Gaze/CameraDot.cs
@@ -2,30 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class CameraDot : MonoBehaviour
 {
-  public float dot;
-  public float downThreshold = -0.7f;
+    public float dot;
+    public float downThreshold = -0.7f;
+    public float timer;
+    public float triggerTime = 3;
+    public Image slider;
 
-  public bool isLookingDown = false;
+    public bool isLookingDown = false;
 
-  public UnityEvent onLookDownEvent;
+    public UnityEvent onLookDownEvent;
 
-  bool isTriggered;
+    bool isTriggered;
+    bool triggered;
 
-  void Update()
-  {
-    dot = Vector3.Dot(Vector3.up, transform.forward);
-    isLookingDown = (dot < downThreshold);
-
-    if (isLookingDown && !isTriggered)
+    void Update()
     {
-      isTriggered = true;
-      onLookDownEvent.Invoke();
+        dot = Vector3.Dot(Vector3.up, transform.forward);
+        isLookingDown = (dot < downThreshold);
+
+        if (isLookingDown && !isTriggered)
+        {
+            isTriggered = true;
+        }
+
+        if (!isLookingDown && isTriggered)
+        {
+            isTriggered = false;
+        }
+
+        if (isTriggered && !triggered && timer <= 3)
+        {
+            slider.enabled = true;
+            timer += Time.deltaTime;
+            print((timer / triggerTime));
+            slider.fillAmount = (timer / triggerTime);
+        }
+        else if(isTriggered && !triggered)
+        {
+            triggered = false;
+            onLookDownEvent.Invoke();
+        }
+        else
+        {
+            slider.enabled = false;
+            timer = 0;
+        }
     }
 
-    if (!isLookingDown && isTriggered)
-      isTriggered = false;
-  }
+    public void ResetVarables()
+    {
+        triggered = false;
+        isTriggered = false;
+    }
 }

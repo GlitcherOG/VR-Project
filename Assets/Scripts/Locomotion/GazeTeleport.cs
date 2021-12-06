8c3a9702e00c1179287527bb2c109b3d1e50eba5
Jordy3D <fchetar@gmail.com>
2021-12-06 13:51:10 +1100

    Fuck

--- Assets/Scripts/Locomotion/GazeTeleport.cs
+++ Assets/Scripts/Locomotion/GazeTeleport.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GazeTeleport : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class GazeTeleport : MonoBehaviour
   public bool isEnabled = true;
   public MeshCollider col;
   public MeshRenderer rend;
+
+  public Image fade; 
 
   private void OnDrawGizmos()
   {
@@ -32,6 +35,35 @@ public class GazeTeleport : MonoBehaviour
 
     Transform cam = Camera.main.transform;
     cam.localEulerAngles = new Vector3(cam.localRotation.x, 0, cam.localRotation.z); // Resets rotation relative to rig
+  }
+
+  public void OnTeleport()
+  {
+    StartCoroutine(nameof(Teleport));
+  }
+
+  IEnumerator Teleport()
+  {
+    float t = 0;
+    Color alpha = new Color(0, 0, 0, 0);
+    while (t < 100)
+    {
+      Debug.Log(t);
+      t += Time.deltaTime;
+      print(Color.Lerp(alpha, Color.black, t / 100));
+      fade.color = Color.Lerp(alpha, Color.black, t/100);
+    }
+    print("Teleporting!");
+    TeleportToPoint();
+    t = 0;
+    while (t < 100)
+    {
+      print("Fading out!");
+      t += Time.deltaTime;
+      fade.color = Color.Lerp(Color.black, alpha, t/100);
+    }
+
+    yield return new WaitForSeconds(0);
   }
 
   private void OnTriggerEnter(Collider other) => SetEnabled(false);
